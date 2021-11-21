c135a1ae77be4e4e68cf3d4e52c7a39d547fa3bc
Danny Padilla <dpadilla010@gmail.com>
2021-11-21 13:57:43 -0800

    ADD - query db using app local scope

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', async function(req, res, next) {
+  let db_collection_name = req.app.locals.db_collection;
+  let users = await req.app.locals.db.collection(db_collection_name);
+
+  res.json( await users.find().toArray() );
+
 });
 
 module.exports = router;
