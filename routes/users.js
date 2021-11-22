015898d94c9f4bbc7327b37ca899a207d9c1b515
Danny Padilla <dpadilla010@gmail.com>
2021-11-21 16:57:14 -0800

    ADD - login endpoint using jwt

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,34 @@
 var express = require('express');
 var router = express.Router();
+var jwt = require("jsonwebtoken");
+
+// login endpoint
+router.post("/login", function(req, res) {
+  // assume user sends user/pw in request body
+  // (should use db to get user information )
+  let username = req.body.username;
+  let password = req.body.password;
+  let secret = process.env.JWT_SECRET || "hello";
+  if (username == "user" && password == "pw") {
+
+    // first arg: users object with information that you want to use later for auth purposes
+    // second arg: secret used to encrypt the jwt token
+    var token = jwt.sign({
+      username: "user",
+      isAdmin: false
+    }, secret);
+
+    res.json({token});
+
+  } else {
+
+    res.status(401).json({
+      message: "authentication failed"
+    });
+
+  }
+});
+
 
 /* GET users listing. */
 router.get('/', async function(req, res, next) {
